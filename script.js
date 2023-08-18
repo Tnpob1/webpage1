6c86d6a7d60792dd09027c673bbac36d594ee509
Tnpob1 <titleetinnapob@gmail.com>
2023-08-19 00:16:29 +0700

    first commit

--- script.js
+++ script.js
@@ -1,103 +1,106 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const openCartBtn = document.getElementById('openCartBtn');
-    const cartModal = document.getElementById('cartModal');
-    const closeBtn = cartModal.querySelector('.close');
-    const cartItems = document.getElementById('cartItems');
-    const cartTotal = document.getElementById('cartTotal');
-    const addToCartButtons = document.querySelectorAll('.add-to-cart');
-  
-    // New: Add and subtract buttons
-    const cartContent = document.querySelector('.modal-content');
-    const cartItemList = document.getElementById('cartItemList');
-  
-    const cart = [];
+document.addEventListener("DOMContentLoaded", function () {
+  const openCartBtn = document.getElementById("openCartBtn");
+  const cartModal = document.getElementById("cartModal");
+  const closeBtn = cartModal.querySelector(".close");
+  const cartItems = document.getElementById("cartItems");
+  const cartTotal = document.getElementById("cartTotal");
+  const addToCartButtons = document.querySelectorAll(".add-to-cart");
 
-    
+  // New: Add and subtract buttons
+  const cartContent = document.querySelector(".modal-content");
+  const cartItemList = document.getElementById("cartItemList");
 
-  
-    function updateCart() {
-      cartItems.innerHTML = '';
-      let total = 0;
-  
-      cart.forEach(item => {
-        const li = document.createElement('li');
-        li.innerHTML = `
+  const cart = [];
+
+  function updateCart() {
+    cartItems.innerHTML = "";
+    let total = 0;
+
+    cart.forEach((item) => {
+      const li = document.createElement("li");
+      li.innerHTML = `
           <span>${item.name} - $${item.price.toFixed(2)}</span>
           <button class="decrement" data-product="${item.name}">-</button>
           <span class="quantity">${item.quantity}</span>
           <button class="increment" data-product="${item.name}">+</button>
         `;
-        cartItems.appendChild(li);
-        total += item.price * item.quantity;
-      });
-  
-      const discountThreshold = 1000;
-      if (total > discountThreshold) {
-        total *= 0.9;
-      }
-  
-      cartTotal.textContent = total.toFixed(2);
-  
-      // Attach event listeners for increase and decrease buttons
-      const decrementButtons = document.querySelectorAll('.decrement');
-      const incrementButtons = document.querySelectorAll('.increment');
-  
-      decrementButtons.forEach(button => {
-        button.addEventListener('click', function () {
-          const productName = button.getAttribute('data-product');
-          const cartItem = cart.find(item => item.name === productName);
-  
-          if (cartItem && cartItem.quantity > 1) {
-            cartItem.quantity--;
-            updateCart();
-          }
-        });
-      });
-  
-      incrementButtons.forEach(button => {
-        button.addEventListener('click', function () {
-          const productName = button.getAttribute('data-product');
-          const cartItem = cart.find(item => item.name === productName);
-  
-          if (cartItem) {
-            cartItem.quantity++;
-            updateCart();
-          }
-        });
-      });
+      cartItems.appendChild(li);
+      total += item.price * item.quantity;
+    });
+
+    const discountThreshold = 1000;
+    if (total > discountThreshold) {
+      total *= 0.9;
     }
-  
-    addToCartButtons.forEach(button => {
-      button.addEventListener('click', function () {
-        const product = button.getAttribute('data-product');
-        const name = button.parentElement.parentElement.querySelector('.detail p:first-child').textContent;
-        const price = parseFloat(button.parentElement.parentElement.querySelector('.detail p:last-child').textContent.slice(0,-4));
-  
-        // Check if the product is already in the cart
-        const existingItem = cart.find(item => item.name === name);
-        if (existingItem) {
-          existingItem.quantity++;
-        } else {
-          cart.push({ name, price, quantity: 1 });
+
+    cartTotal.textContent = total.toFixed(2);
+
+    // Attach event listeners for increase and decrease buttons
+    const decrementButtons = document.querySelectorAll(".decrement");
+    const incrementButtons = document.querySelectorAll(".increment");
+
+    decrementButtons.forEach((button) => {
+      button.addEventListener("click", function () {
+        const productName = button.getAttribute("data-product");
+        const cartItem = cart.find((item) => item.name === productName);
+
+        if (cartItem && cartItem.quantity > 1) {
+          cartItem.quantity--;
+          updateCart();
         }
-        updateCart();
       });
     });
-  
-    openCartBtn.addEventListener('click', function () {
-      cartModal.style.display = 'block';
+
+    incrementButtons.forEach((button) => {
+      button.addEventListener("click", function () {
+        const productName = button.getAttribute("data-product");
+        const cartItem = cart.find((item) => item.name === productName);
+
+        if (cartItem) {
+          cartItem.quantity++;
+          updateCart();
+        }
+      });
     });
-  
-    closeBtn.addEventListener('click', function () {
-      cartModal.style.display = 'none';
-    });
-  
-    window.addEventListener('click', function (event) {
-      if (event.target === cartModal) {
-        cartModal.style.display = 'none';
+  }
+
+  addToCartButtons.forEach((button) => {
+    button.addEventListener("click", function () {
+      const product = button.getAttribute("data-product");
+      const name = button.parentElement.parentElement.querySelector(
+        ".detail p:first-child"
+      ).textContent;
+      const price = parseFloat(
+        button.parentElement.parentElement
+          .querySelector(".detail p:last-child")
+          .textContent.slice(0, -4)
+      );
+
+      // Check if the product is already in the cart
+      const existingItem = cart.find((item) => item.name === name);
+      if (existingItem) {
+        existingItem.quantity++;
+      } else {
+        cart.push({ name, price, quantity: 1 });
       }
+      updateCart();
     });
   });
+
+  openCartBtn.addEventListener("click", function () {
+    cartModal.style.display = "block";
+  });
+
+  closeBtn.addEventListener("click", function () {
+    cartModal.style.display = "none";
+  });
+
+  window.addEventListener("click", function (event) {
+    if (event.target === cartModal) {
+      cartModal.style.display = "none";
+    }
+  });
+});
 
 const carousel = document.querySelector(".carousel");
 const prevButton = document.getElementById("prevBtn");
@@ -122,3 +125,11 @@ function nextSlide() {
 prevButton.addEventListener("click", prevSlide);
 nextButton.addEventListener("click", nextSlide);
 
+const handleSubmit = (e) => {
+  e.preventDefault();
+  alert("ซื้อสำเร็จ");
+};
+
+const form = document.getElementById("form");
+
+form.addEventListener("submit", handleSubmit);
